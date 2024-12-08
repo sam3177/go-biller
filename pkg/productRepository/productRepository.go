312063d453c79108972defeac7236b2267a06521
Silviu Samson <silviu.samson@bytex.ro>
2024-12-08 15:51:44 +0200

    feat: add NewProduct function, in which we validate product's fields

--- pkg/productRepository/productRepository.go
+++ pkg/productRepository/productRepository.go
@@ -5,10 +5,57 @@ import (
 	"fmt"
 	"math"
 	"slices"
+
+	"github.com/google/uuid"
 )
 
 type LocalProductRepository struct {
 	products []utils.Product
+}
+
+func NewProduct(
+	id string,
+	name string,
+	unitPrice float64,
+	unitType utils.UnitType,
+	stock float64,
+) *utils.Product {
+
+	//id check
+	var productId string
+	if id != "" {
+		productId = id
+	} else {
+		productId = uuid.NewString()
+	}
+
+	// name check
+	if name == "" {
+		panic("Product name is mandatory.")
+	}
+
+	// unit price check
+	if unitPrice <= 0 {
+		panic("Product unitPrice must be greater than 0.")
+	}
+
+	// unit type check
+	if unitType != utils.UnitPiece && unitType != utils.UnitKg {
+		panic("Product unitType must be 'piece' or 'kg'.")
+	}
+
+	// stock check
+	if stock < 0 {
+		panic("Product stock must be 0 or greater.")
+	}
+
+	return &utils.Product{
+		Id:        productId,
+		Name:      name,
+		UnitPrice: unitPrice,
+		UnitType:  unitType,
+		Stock:     stock,
+	}
 }
 
 func NewLocalProductRepository(products []utils.Product) *LocalProductRepository {
@@ -50,20 +97,25 @@ func (repo *LocalProductRepository) IsEnoughProductInStock(id string, desiredQua
 	return product.Stock >= desiredQuantity
 }
 
-func (repo *LocalProductRepository) UpdateStock(id string, quantity float64) {
+func (repo *LocalProductRepository) UpdateStock(id string, quantity float64) error {
 	product, error := repo.GetProductById(id)
 
 	if error != nil {
-		fmt.Println(error.Error())
-		return
+		return error
 	}
 
 	if !repo.CanProductHaveDecimalStock(id) && quantity != math.Floor(quantity) {
-		fmt.Println("This product is sold by piece, so you can not ask for a decimal point quantity.")
-		return
+		return fmt.Errorf("this product is sold by piece, so a decimal point quantity is not valid in this case")
+	}
+
+	if quantity < 0 && !repo.IsEnoughProductInStock(id, quantity*-1) {
+
+		return fmt.Errorf("the available stock for this product is %f, but you requested %f", product.Stock, quantity*-1)
 	}
 
 	product.Stock += quantity
+
+	return nil
 }
 
 func (repo *LocalProductRepository) CanProductHaveDecimalStock(id string) bool {
