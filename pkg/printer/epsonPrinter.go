afcd9395a31adf8b45fce07be57808f113e73577
Silviu Samson <silviu.samson@bytex.ro>
2024-12-19 18:04:49 +0200

    feat: update printer structs

--- pkg/printer/epsonPrinter.go
+++ pkg/printer/epsonPrinter.go
@@ -7,19 +7,21 @@ import (
 )
 
 type EpsonPrinter struct {
-	name string
+	name      string
+	rowLength int
 }
 
 func NewEpsonPrinter(printerName string) *EpsonPrinter {
 	return &EpsonPrinter{
-		name: printerName,
+		name:      printerName,
+		rowLength: 46, // hardcoded on purpose, it will not change for this printer
 	}
 }
 
-func (printer *EpsonPrinter) Print(data string) {
+func (printer *EpsonPrinter) Print(data bytes.Buffer) {
 	cmd := exec.Command("lp", "-d", printer.name, "-o", "raw")
 
-	cmd.Stdin = printer.getContentBuffer(data)
+	cmd.Stdin = &data
 	err := cmd.Run()
 
 	if err != nil {
@@ -27,19 +29,6 @@ func (printer *EpsonPrinter) Print(data string) {
 	}
 }
 
-func (printer *EpsonPrinter) getContentBuffer(data string) *bytes.Buffer {
-	var buffer bytes.Buffer
-
-	buffer.Write([]byte{0x1B, 0x40}) // ESC @ (initialize)
-
-	buffer.Write([]byte{0x1D, 0x4C, 0x0A, 0x00}) // 10 dots left margin (1 character width)
-
-	buffer.Write([]byte{0x1B, 0x61, 0x00}) // Left align
-
-	buffer.WriteString(data)
-
-	buffer.Write([]byte{0x1B, 0x64, 0x03}) // Feed 3 lines
-	buffer.Write([]byte{0x1D, 0x56, 0x00}) // Cut the paper
-
-	return &buffer
+func (printer *EpsonPrinter) GetRowLength() int {
+	return printer.rowLength
 }
