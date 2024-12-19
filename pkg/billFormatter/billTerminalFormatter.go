22ab539fca18f4e3124d6647a859fc7a925f77b3
Silviu Samson <silviu.samson@bytex.ro>
2024-12-19 18:07:14 +0200

    feat: create BillTerminalFormatter

---
+++ pkg/billFormatter/billTerminalFormatter.go
@@ -0,0 +1,38 @@
+package billFormatter
+
+import (
+	"biller/pkg/utils"
+	"bytes"
+	"fmt"
+)
+
+type BillTerminalFormatter struct {
+	BillFormatter
+}
+
+func NewBillTerminalFormatter() *BillTerminalFormatter {
+	return &BillTerminalFormatter{}
+}
+
+func (formatter *BillTerminalFormatter) FormatBill(billData utils.BillData, rowLength int) bytes.Buffer {
+	var buffer bytes.Buffer
+
+	billTitle := formatter.getBillTitle()
+	buffer.WriteString(fmt.Sprintf("%*s", (rowLength+len(billTitle))/2, billTitle))
+
+	buffer.WriteString(fmt.Sprintf("Table name: %v \n", billData.TableName))
+
+	buffer.WriteString(formatter.makeDottedLine(rowLength))
+
+	for _, product := range billData.Products {
+		buffer.WriteString(formatter.formatBillProduct(product, rowLength))
+	}
+
+	buffer.WriteString(formatter.makeDottedLine(rowLength))
+
+	buffer.WriteString(formatter.formatSubtotalAndTip(billData, rowLength))
+
+	buffer.WriteString(formatter.makeFooterLine("Total", billData.Total, rowLength))
+
+	return buffer
+}
