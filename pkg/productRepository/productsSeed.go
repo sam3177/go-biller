46944f830032622d371dea0aaf4889d47667ffb4
Silviu Samson <silviu.samson@bytex.ro>
2025-01-24 09:06:08 +0100

    feat: add VAt to items in products seed file

--- pkg/productRepository/productsSeed.go
+++ pkg/productRepository/productsSeed.go
@@ -5,8 +5,9 @@ import (
 )
 
 var ProductsSeed []utils.Product = []utils.Product{
-	{Name: "Banana", UnitPrice: 1, UnitType: "kg", Stock: 40},
-	{Name: "Apple", UnitPrice: 2, UnitType: "kg", Stock: 30},
-	{Name: "Orange", UnitPrice: 3, UnitType: "kg", Stock: 10},
-	{Name: "Avocado", UnitPrice: 2.19, UnitType: "piece", Stock: 34},
+	{Name: "Banana", UnitPrice: 1, UnitType: "kg", Stock: 40, VATCategory: utils.A},
+	{Name: "Apple", UnitPrice: 2, UnitType: "kg", Stock: 30, VATCategory: utils.A},
+	{Name: "Orange", UnitPrice: 3, UnitType: "kg", Stock: 10, VATCategory: utils.A},
+	{Name: "Avocado", UnitPrice: 2.19, UnitType: "piece", Stock: 34, VATCategory: utils.A},
+	{Name: "Toilet Paper", UnitPrice: .1, UnitType: "piece", Stock: 34, VATCategory: utils.B},
 }
